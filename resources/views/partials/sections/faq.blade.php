f4b83854c7d186c99afd2e19a2daa8eba6d8bb81
Andrew Schmelyun <schmelyun@gmail.com>
2020-10-24 03:30:25 -0400

    Modifies h5 to h3 for sequentially descending headings

--- resources/views/partials/sections/faq.blade.php
+++ resources/views/partials/sections/faq.blade.php
@@ -1,7 +1,7 @@
 <section class="text-lg text-gray-600 leading-relaxed mb-6">
     <h2 class="text-3xl text-gray-800 font-bold pb-6 leading-snug lg:leading-relaxed">Frequently Asked <span class="fancy-underline">Questions</span></h2>
     @foreach($faqs as $faq)
-        <h5 class="text-lg text-gray-800 font-semibold pb-2 leading-none italic">{{ $faq->question }}</h5>
+        <h3 class="text-lg text-gray-800 font-semibold pb-2 leading-none italic">{{ $faq->question }}</h3>
         <p class="pb-8">{{ $faq->answer }}</p>
     @endforeach
 </section>
