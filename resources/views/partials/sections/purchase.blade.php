02fe942298791ce31f2d2a844fed11b44ee7c5ae
Andrew Schmelyun <schmelyun@gmail.com>
2020-11-21 04:32:13 -0500

    Updates

--- resources/views/partials/sections/purchase.blade.php
+++ resources/views/partials/sections/purchase.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-gray-200 rounded-xl pt-4 pb-6 px-6">
         <h2 class="text-3xl text-gray-800 font-bold pb-3 leading-snug lg:leading-relaxed">Buy the <span class="fancy-underline">full course</span></h2>
         <p class="text-gray-600 pb-4">You'll receive <strong class="text-gray-800 font-semibold">24 videos</strong> in HD, as well as <strong class="text-gray-800 font-semibold">full English transcripts</strong> and <strong class="text-gray-800 font-semibold">complete source code</strong> for every video.</p>
-        <p class="text-gray-600 pb-4">The full course releases on <strong class="text-gray-800 font-semibold">November 20th</strong>, pre-order now to save $10! After purchasing, you'll be notified when all of the materials will be available for you to download on the release date.</p>
+        <p class="text-gray-600 pb-4">The full course releases on <strong class="text-gray-800 font-semibold">November 30th</strong>, pre-order now to save $10! After purchasing, you'll be notified when all of the materials will be available for you to download on the release date.</p>
         <div class="text-center">
             <a href="https://gumroad.com/l/cwYKw" data-gumroad-single-product="true" rel="noreferrer" class="inline-block bg-gradient-to-br from-indigo-800 to-blue-700 text-white uppercase font-semibold py-3 px-8 text-lg rounded-md transition duration-300 ease-in-out hover:shadow-md">👉 Pre-order<span class="hidden lg:inline"> Now</span> 👈</a>
         </div>
